ac7fbe5861722cf5636bd6186935e751eaa428bd
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-09 20:58:08 +0300

    Add Project-07

---
+++ Project-07/Project-07/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project-07
+//
+//  Created by Ibragim Akaev on 27/12/2020.
+//
+
+import Foundation
+
+struct Petitions: Codable, Equatable {
+    var results: [Petition]
+}
