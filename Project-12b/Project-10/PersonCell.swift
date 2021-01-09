e09247cdb31b875d782245408fde6e42e66d071b
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-09 20:59:57 +0300

    Add Project-12b

---
+++ Project-12b/Project-10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project-10
+//
+//  Created by Ibragim Akaev on 30/12/2020.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
