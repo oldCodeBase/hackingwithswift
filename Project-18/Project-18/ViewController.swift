579ed5212d8fd883698a2da79bb4cc81ca762496
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-19 19:35:07 +0300

    add Project-18

---
+++ Project-18/Project-18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project-18
+//
+//  Created by Ibragim Akaev on 19/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
